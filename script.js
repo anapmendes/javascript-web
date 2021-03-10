725afcfae6464e3313b51540c375657761a4f76e
anapmendes <anapsmendess@gmail.com>
2021-03-10 10:46:16 -0300

    Exercicios para a turma

--- script.js
+++ script.js
@@ -14,14 +14,31 @@ function goFollow() {
     }
 }
 
+function clearSug() {
+    let listProfile = document.getElementById('firstProfile');
+    listProfile.style.display = 'none';
+}
+
+//Ao clicar no follow das sugestões, aumentar numero de Following e depois de 5s dar um display none na lista
+
+//No refresh trocar o número e no comments trocar o número, como no like
+
 function like() {
     let tweetLiked = document.getElementById('like');
+    let favorites = document.getElementById('favoritesNumber');
+    let likes = document.getElementById('likeNumbers')
 
-    tweetLiked.src = tweetLiked.src.indexOf('heart-color') < 0 ? 'images/heart-color.svg' : 'images/heart.svg';
+    // tweetLiked.src = tweetLiked.src.indexOf('heart-color') < 0 ? 'images/heart-color.svg' && ++favorites.innerText : 'images/heart.svg';
 
-    // if (tweetLiked.src.indexOf('heart-color') == -1) {
-    //     tweetLiked.src = 'images/heart-color.svg';
-    // } else {
-    //     tweetLiked.src = 'images/heart.svg';
-    // }
+    if (tweetLiked.src.indexOf('heart-color') == -1) {
+        tweetLiked.src = 'images/heart-color.svg';
+
+        ++favorites.innerText;
+        ++likes.innerText;
+    } else {
+        tweetLiked.src = 'images/heart.svg';
+
+        --favorites.innerText;
+        --likes.innerText;
+    }
 }
